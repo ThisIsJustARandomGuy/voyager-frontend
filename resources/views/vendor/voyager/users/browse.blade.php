195bb2149fe02da7e08e340de5d74cba296692ac
Lukas Hofer <l.hofer@conecto.at>
2022-09-23 10:39:51 +0200

    Fix escaping in templates

--- resources/views/vendor/voyager/users/browse.blade.php
+++ resources/views/vendor/voyager/users/browse.blade.php
@@ -38,12 +38,12 @@
                                 <div id="search-input">
                                     <select id="search_key" name="key">
                                         @foreach($searchable as $key)
-                                            <option value="{{ $key }}" @if($search->key == $key){{ 'selected' }}@endif>{{ ucwords(str_replace('_', ' ', $key)) }}</option>
+                                            <option value="{{ $key }}" @if($search->key == $key) selected @endif>{{ ucwords(str_replace('_', ' ', $key)) }}</option>
                                         @endforeach
                                     </select>
                                     <select id="filter" name="filter">
-                                        <option value="contains" @if($search->filter == "contains"){{ 'selected' }}@endif>contains</option>
-                                        <option value="equals" @if($search->filter == "equals"){{ 'selected' }}@endif>=</option>
+                                        <option value="contains" @if($search->filter == "contains") selected @endif>contains</option>
+                                        <option value="equals" @if($search->filter == "equals") selected @endif>=</option>
                                     </select>
                                     <div class="input-group col-md-12">
                                         <input type="text" class="form-control" placeholder="{{ __('voyager::generic.search') }}" name="s" value="{{ $search->value }}">
